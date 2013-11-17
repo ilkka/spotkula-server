3b56143c74fdccd50445589182481e306ecb600e
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2013-11-17 16:59:39 +0200

    Implement track skipping
    Posting to /skip will cause an AppleScript call to make spotify skip to
    the next track.

--- server/index.js
+++ server/index.js
@@ -10,6 +10,14 @@ app.get('/', function (request, response) {
   response.send('ok');
 });
 
+app.post('/skip', function (request, response) {
+  spotify.next(function () {
+    response.set('Connection', 'close');
+    console.log("Skipped to next track");
+    response.send('ok');
+  });
+});
+
 app.listen(3000);
 console.log("Listening on port 3000");
 
